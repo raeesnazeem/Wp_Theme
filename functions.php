dcf824979d3c9186b9ab6f2c89061fa06ec74db3
raeesnazeem <91611445+raeesnazeem@users.noreply.github.com>
2025-03-14 23:37:51 +0530

    functions.php modified

--- functions.php
+++ functions.php
@@ -3,4 +3,4 @@ function medspa_files() {
     wp_enqueue_style('medspa_main_styles', get_stylesheet_uri());
 }
 
-add_action('wp_enqueue_scripts', 'medspa_files');
+add_action('wp_enqueue_scripts', 'medspa_files'); //medspa_files is the name of the function that wordpress will call when required. wp_enqueue_scripts is the wordpress hook   
