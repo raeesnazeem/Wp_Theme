e3a09c6b1342fd1bdc9f4439e38fd9086cee73c3
raeesnazeem <91611445+raeesnazeem@users.noreply.github.com>
2025-03-17 23:14:30 +0530

    small change

--- functions.php
+++ functions.php
@@ -8,16 +8,21 @@ function medspa_files() {
 
 add_action('wp_enqueue_scripts', 'medspa_files'); //This is to load the css and javascript. medspa_files is the name of the function that we created and wordpress will call when required. The function is declared above. wp_enqueue_scripts is the wordpress hook 
 
-// function medspa_additional_features() {
-//     add_theme_support('title_tag');
-// }
+function medspa_additional_features() {
+    register_nav_menu('headerMenuLocation', 'Header Menu Location');
 
-
-// add_action('after_setup_theme', 'medspa_additional_features'); //This is to add theme features. hook os after_theme_setup and medspa_faetures is custom function we define above.
-
-add_action('after_setup_theme', function() {
+    //title-tag support
     remove_action('wp_head', '_wp_render_title_tag', 1);
     add_action('wp_head', function() {
         echo '<title>' . wp_get_document_title() . '</title>';
     });
-});
+    // add_theme_support('title_tag'); - this was not working so added the above lines instead
+}
+
+
+
+add_action('after_setup_theme', 'medspa_additional_features'); //This is to add theme features. hook os after_theme_setup and medspa_faetures is custom function we define above.
+
+// add_action('after_setup_theme', function() {
+    
+// });
